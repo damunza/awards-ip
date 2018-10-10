696dddd1a514e82fddfb00d8e95ea2933d7c69ba
damunza <danidamunza@gmail.com>
2018-10-10 12:26:41 +0300

    created the rating model

--- app/models.py
+++ app/models.py
@@ -1,19 +1,19 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-#
-# # Create your models here.
-#
-# class Rate(models.Model):
-#     rater = models.ForeignKey(User,on_delete=models.CASCADE)
-#     task = models.ForeignKey('app.Project',on_delete=models.CASCADE,related_name='rates')
-#     design = models.PositiveIntegerField(default=0)
-#     usability = models.PositiveIntegerField(default=0)
-#     content = models.PositiveIntegerField(default=0)
-#
-#     def __str__(self):
-#         return self.rater.username
-#
-#
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Rate(models.Model):
+    rater = models.ForeignKey(User,on_delete=models.CASCADE)
+    task = models.ForeignKey('app.Project',on_delete=models.CASCADE,related_name='rates')
+    design = models.PositiveIntegerField(default=0)
+    usability = models.PositiveIntegerField(default=0)
+    content = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.rater.username
+
+
 # class Project(models.Model):
 #     title = models.CharField(max_length = 60)
 #     homepage = models.ImageField(upload_to = 'images/')
