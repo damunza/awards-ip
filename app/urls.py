231e6ef7567a28aa27cff76662c2546b6af97887
damunza <danidamunza@gmail.com>
2018-10-11 13:59:25 +0300

    displayed the profile of the user on the profile page

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url('^$',views.home, name = 'home'),
+    url(r'^profile/(\w+)',views.profile,name = 'profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import *
-from django.contrib.auth.decorators import login_required.
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
