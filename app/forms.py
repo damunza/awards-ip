8dea29b4d1de238d2b03898b32860af6672d3dbd
damunza <danidamunza@gmail.com>
2018-10-12 11:55:07 +0300

    created a form for rating

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,8 @@ class NewProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['by','rating']
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        exclude = ['rater','task','average']
